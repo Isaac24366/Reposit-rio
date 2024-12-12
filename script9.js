b793f54dbcb2bd934766657b8417fba069407496
ISAAC <barros.isaac@escola.pr.gov.br>
2024-12-12 15:04:20 -0300

    Update script9.js

--- script9.js
+++ script9.js
@@ -1,13 +1,13 @@
-let input = document.getElementById("input");
+//let input = document.getElementById("input");
 function n(value){
-    //document.getElementById("input").value += value;
-    input.value += value;
+    document.getElementById("input").value += value;
+    //input.value += value;
 }
 function c(){
-    //document.getElementById("input").value = "";
-    input.value = "";
+    document.getElementById("input").value = "";
+    //input.value = "";
 }
 function calcular(){
-    //document.getElementById("input").value = eval(document.getElementById("input").value);
-    input.value = eval(input.value);
+    document.getElementById("input").value = eval(document.getElementById("input").value);
+    //input.value = eval(input.value);
 }
